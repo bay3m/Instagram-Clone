f30d549a667c71f0f961890e011d5e1b6f482f20
bay3m <hansonsiwenbrian@gmail.com>
2023-10-12 16:24:04 +0700

    main

--- application/models.py
+++ application/models.py
@@ -3,37 +3,47 @@ from datetime import datetime
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    active = db.Column(db.Boolean, nullable=False)
-    username = db.Column(db.String(31), nullable=False)
-    profile_photo_url = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id            = db.Column(db.Integer, primary_key=True)
+    username      = db.Column(db.String(128), nullable=False)
+    password      = db.Column(db.String(128), nullable=False)
+    fullname      = db.Column(db.String(128), nullable=False)
+    email         = db.Column(db.String(128), nullable=False)
+    profile_pic   = db.Column(db.String(128), default="default.jpg")
+    bio           = db.Column(db.String(128))
+    join_date     = db.Column(db.DateTime, default=datetime.utcnow)
+    status        = db.Column(db.Boolean, default=True)
                          
-class Relationship(db.Model):
-    __tablename__ = "relationships"
-    id = db.Column(db.Integer, primary_key=True)
-    follower = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    following = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    status = db.Column(db.Boolean, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+class Relation(db.Model):
+    __tablename__ = "relations"
+    id            = db.Column(db.Integer, primary_key=True)
+    id_follower   = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    id_following  = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    status        = db.Column(db.Boolean, nullable=False)
+    relation_date = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Post(db.Model):
     __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key=True)
-    created_by = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    image_url = db.Column(db.String(255), nullable=False)
-    caption = db.Column(db.String(1023), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id            = db.Column(db.Integer, primary_key=True)
+    author_id     = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    photo         = db.Column(db.String(255), nullable=False)
+    caption       = db.Column(db.String(1023), nullable=False)
+    status        = db.Column(db.Boolean, nullable=False)
+    post_date     = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Comment(db.Model):
     __tablename__ = "comments"
-    id = db.Column(db.Integer, primary_key=True)
-    post = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
-    content = db.Column(db.String(1023), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id            = db.Column(db.Integer, primary_key=True)
+    text          = db.Column(db.Text, nullable=False)
+    commenter_id  = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id       = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    hidden        = db.Column(db.Boolean, default=False)
+    comment_date  = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class Like(db.Model):
     __tablename__ = "likes"
-    id = db.Column(db.Integer, primary_key=True)
-    post = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id            = db.Column(db.Integer, primary_key=True)
+    user_id       = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id       = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    status        = db.Column(db.Boolean, default=True)
+    like_date     = db.Column(db.DateTime, default=datetime.utcnow)
